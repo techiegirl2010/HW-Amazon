3639820316883d6595f01a1d5d75f5d9177af696
Joyce Buettner <iamtechiegirl@cox.net>
2019-07-21 21:33:39 -0700

    files fixed

--- bamazon.sql
+++ bamazon.sql
@@ -1,5 +1,5 @@
 -- Drops the bamazon_db if it exists currently --
-DROP DATABASE IF EXISTS bamazon_db;
+DROP DATABASE IF EXISTS bamazon;
 
 -- Creates the bamazon_db database --
 CREATE DATABASE bamazon_db;
@@ -11,3 +11,52 @@ USE bamazon_db;
 CREATE TABLE bamazon (
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(30) NOT NULL,
+
+-- Create Table inside database called products.
+-- The products table should have each of the following columns:
+-- item_id (unique id for each product)
+-- product_name (Name of product)
+-- department_name
+-- price (cost to customer)
+-- stock_quantity (how much of the product is available in stores)
+CREATE TABLE products(
+    item_id INTEGER(4) NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30), NOT NULL,
+    price DECIMAL(10,2), NOT NULL,
+    stock_quantity INTEGER(10), NOT NULL
+)
+
+
+-- Insert rows containing data in all named columns
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+
+
