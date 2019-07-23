d5e9ab55072563bf8768fce4f2d1077f0c2421ee
Joyce Buettner <iamtechiegirl@cox.net>
2019-07-22 23:02:30 -0700

    updates without connection-not finished

--- bamazon.js
+++ bamazon.js
@@ -1,14 +1,16 @@
 var mysql = require("mysql");
 var inqiurer = require("inquirer");
+require("console.table");
 
 // Create connection info for database
 var connection = mysql.createConnection({
     host: "localhost",
-    port: "3307",
+    port: "3306",
     username: "root",
     password: "root",
     database: "bamazon_db"
 })
+
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
@@ -16,3 +18,59 @@ connection.connect(function (err) {
     // run the start function after the connection is made to prompt the user
     start();
 });
+function start(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if(err) throw err;
+
+        console.table(res);
+        promptForId(res);
+    })
+}
+
+function promptForId(inventory){
+    inquirer.prompt([{
+        type: "input",
+        name: "id",
+        message: "what is the id of the item you would like to buy?"
+    }]).then(function(answer){
+        var userId = parseInt(answer.id);
+        var product = checkInventory(userId, inventory);
+        if(product){
+            promptForQuantity(product);
+        }
+        else{
+            console.log("The item is not in the inventory");
+            start();
+        }
+    })
+}
+function checkInventory(userId,inventory){
+    for(var i=0; i<inventory.length; i++){
+        if(inventory[i].item_id == userId){
+            return inventory[i];
+        }
+    }
+    return null;
+}
+function promptForQuantity(product){
+    inquirer.prompt([{
+        type: "input",
+        name: "quantity",
+        message: "how many would you like to buy ?"
+    }]).then(function(answer){
+        var quantity = parseInt(answer.quantity);
+        if(quantity < product.stock_quantity){
+            makePurchase(product, quantity);
+        }
+        else{
+            console.log("Insufficient quantity");
+            start();
+        }
+    })
+}
+function makePurchase(product, quantity){
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",[quantity, product.item_id], function(err, res){
+        console.log("successfully purchased " +quantity + " " +product.product_name  +"for price: " +product.price);
+        start();
+    })
+}
--- bamazon.sql
+++ bamazon.sql
@@ -1,16 +1,11 @@
 -- Drops the bamazon_db if it exists currently --
-DROP DATABASE IF EXISTS bamazon;
+DROP DATABASE IF EXISTS bamazon_db;
 
 -- Creates the bamazon_db database --
 CREATE DATABASE bamazon_db;
 
 -- Makes it so all of the following code will affect bamazon_db --
 USE bamazon_db;
-
--- Creates the table "people" within animals_db --
-CREATE TABLE bamazon (
-  -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(30) NOT NULL,
 
 -- Create Table inside database called products.
 -- The products table should have each of the following columns:
@@ -22,41 +17,55 @@ CREATE TABLE bamazon (
 CREATE TABLE products(
     item_id INTEGER(4) NOT NULL,
     product_name VARCHAR(30) NOT NULL,
-    department_name VARCHAR(30), NOT NULL,
-    price DECIMAL(10,2), NOT NULL,
-    stock_quantity INTEGER(10), NOT NULL
-)
-
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL,
+    primary key(item_id)
+);
 
 -- Insert rows containing data in all named columns
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0100", "Flea collar", "cat", "29.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0101", "Revolution - 6 pack", "cat", "89.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0102", "Friskies fish pate", "cat", "0.60", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0103", "Tiki Cat - chicken shreds", "cat", "1.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0104", "Carrier - blue", "cat", "49.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0105", "Mouse toy", "cat", "1.49", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0106", "Litter box", "cat", "19.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0107", "Laser pointer", "cat", "5.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0108", "Water fountain", "cat", "39.99", "100");
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("0100", "Flea collar", "cat", "29.99", "300");
+VALUES ("0109", "Collar", "cat", "9.99", "100");
+
+-- Displays all inventory
+
+-- Prompts customer to buy
+
+
+-- If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+
+
+-- If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+
+-- If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+
+-- If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
 
 
